288c34e3003a2b48f4e7684f8b6b016e26144989
BlendinaSadiku1 <blendina.sadiku@student.uni-pr.edu>
2026-04-13 21:00:39 +0200

    encyption_client
    enkriptimin DES për klientin

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket from pyDes import *
+
+def get_predefined_key():
+    return b"\x31\x41\x61\x11\x21\x31\x41\x51"
+
+    def encrypt_message(message,key):
+        desi=des(key,ECB, pad=None, padmode=PAD_PKCS5)
+        encrypted_message=desi.encrypt(message)
+        return encrypted_message
