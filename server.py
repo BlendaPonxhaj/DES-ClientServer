c8358b6381f4d2a1a8eb5adaf2012f7df51df3bc
Blenda Ponxhaj <blenda.ponxhaj@student.uni-pr.edu>
2026-04-15 10:05:44 +0200

    feature/server-side: Added DES encryption/decryption functions

--- server.py
+++ server.py
@@ -3,3 +3,15 @@ from pyDes import *
 
 def get_predefined_key():
     return b"\x31\x41\x61\x11\x21\x31\x41\x51"
+
+def encrypt_message(message, key):
+    desi = des(key, ECB, pad=None, padmode=PAD_PKCS5)
+    encrypted_message = desi.encrypt(message)
+    return encrypted_message
+
+def decrypt_message(ciphertext, key):
+    desi = des(key, ECB, pad=None, padmode=PAD_PKCS5)
+    decrypted_message = desi.decrypt(ciphertext)
+    return decrypted_message
+
+
