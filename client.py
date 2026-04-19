180a6f7d3f91fc2384999f39168459d49481aa79
Blenda Ponxhaj <blenda.ponxhaj@student.uni-pr.edu>
2026-04-19 20:52:28 +0200

    refactor: reordered helper functions and improved code spacing

--- client.py
+++ client.py
@@ -6,16 +6,17 @@ def get_predefined_key():
     return b"\x31\x41\x61\x11\x21\x31\x41\x51"
 
 
-def encrypt_message(message, key):
-    desi = des(key, ECB, pad=None, padmode=PAD_PKCS5)
-    encrypted_message=desi.encrypt(message)
-    return encrypted_message
-
-
 def decrypt_message(ciphertext, key):
     desi = des(key, ECB, pad=None, padmode=PAD_PKCS5)
-    decrypted_message=desi.decrypt(ciphertext)
+    decrypted_message = desi.decrypt(ciphertext)
     return decrypted_message
+
+
+def encrypt_message(message, key):
+    desi = des(key, ECB, pad=None, padmode=PAD_PKCS5)
+    encrypted_message = desi.encrypt(message)
+    return encrypted_message
+
 
 
 def client_program():
